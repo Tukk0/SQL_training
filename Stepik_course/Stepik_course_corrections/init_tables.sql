68da8052cbaf4fc8fbe2519d849e5a534b360a91
Тихон Листов <listov.ta@phystech.edu>
2025-08-27 12:56:22 +0300

    Add 'supply' table

--- Stepik_course/Stepik_course_corrections/init_tables.sql
+++ Stepik_course/Stepik_course_corrections/init_tables.sql
@@ -5,3 +5,10 @@ CREATE TABLE book(
   price DECIMAL(8, 2),
   amount INT
 );
+
+CREATE TABLE supply (
+supply_id INT PRIMARY KEY AUTO_INCREMENT,
+title VARCHAR(50),
+author VARCHAR(30),
+price DECIMAL(8, 2),
+amount INT);
